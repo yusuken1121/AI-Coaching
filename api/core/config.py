ba1990c25062061d9f80adacb36debcf068c0674
yusuken1121 <yusuken1121@gmail.com>
2025-06-02 21:22:19 +0900

    Create config for env

--- api/core/config.py
+++ api/core/config.py
@@ -1,1 +1,14 @@
 # 設定管理 (APIキー、Notion DB IDなど)
+
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+class Settings(BaseModel):
+    NOTION_API_KEY: str = os.getenv("NOTION_API_KEY", "default_notion_api_key")
+    NOTION_KPT_DATABASE_ID: str = os.getenv("NOTION_KPT_DATABASE_ID", "default_database_id")
+  
+
+settings = Settings()
